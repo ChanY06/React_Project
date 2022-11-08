8d1cb18a2d7b9749e162f72f3416592e0c1a181b
ChanY06 <69376054+ChanY06@users.noreply.github.com>
2022-11-08 21:32:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const mysql =require('mysql');
 const express =require('express');
+var cors = require('cors');
 const mysqlConnection=require("./connection");
 const restaurentRest=require("./restaurentdb");
 const bookingBook=require("./bookingdb");
@@ -7,9 +8,9 @@ const bookingBook=require("./bookingdb");
 var app = express();
 const bodyparser = require('body-parser');
 app.use(bodyparser.json());
+app.use(cors());
 app.use("/",restaurentRest);
 app.use("/",bookingBook);
 
 
 app.listen(3001,()=>console.log('Express server is runing at port no: 3001'));
-
